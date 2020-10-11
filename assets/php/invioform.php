71a38092d58903c1d4df63f42b82c8936fc74609
bandacittadinadicornedo <62946832+bandacittadinadicornedo@users.noreply.github.com>
2020-10-11 11:39:04 +0200

    Add files via upload

--- assets/php/invioform.php
+++ assets/php/invioform.php
@@ -1,20 +1,20 @@
 <?php
 //Recupero i valori inseriti nel form
-$nome = $_POST['name'];
+$name = $_POST['name'];
 $email = $_POST['email'];
-$messaggio = $_POST['message'];
+$message = $_POST['message'];
 
 //dopo 4 secondi rimando alla home del sito
 $delay = "4";
 $url = "https://dentalprotesi.github.io";
 
 //verifico che tutti i campi siano stati compilati
-if (!$nome || !$email || !$messaggio) {
-	echo 'Tutti i campi del modulo sono obbligatori!?;
+if (!$name || !$email || !$message) {
+	echo 'Tutti i campi del modulo sono obbligatori!';
 	}
 
 //Verifico che il nome non contenga caratteri nocivi
-elseif (!preg_match('/^[A-Aa-z \'-]+$/i',$nome)) {
+elseif (!preg_match('/^[A-Aa-z \'-]+$/i',$name)) {
 	echo 'Il nome contiene caratteri non ammessi';
 	}
 	
@@ -24,22 +24,22 @@ elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	}else{
 		
 // compilo un messaggio combinando i dati recuperati dal form
-$testo = "Nome: " . $nome . "\n"
+$testo = "Nome: " . $name . "\n"
 		. "Email: " . $email . "\n"
-		. "Messaggio: " . $messaggio;
+		. "Messaggio: " . $message;
 		
 // uso la funzione mail di PHP per inviare i dati al mio indirizzo di posta
-mail('lucabee@libero.it, 'Mail da Dental Protesi', $testo);
+mail('lucabee@libero.it', 'Mail da Dental Protesi', $testo);
 
 //Mostro un messaggio di conferma all'utente
-print ">body>
+print "<body>
 <table align='center' bordercolor='#CCCCCC'>
   <tr>
     <td>
 <div align='center'><font face='Verdana, Arial, Helvetica, sans-serif'>
 Grazie per averci contattato</font><br>
 Per tornare all'Home page attendi 4 secondi o <a href='https://dentalprotesi.github.io'>Clicca qui</a></font></div></td>
-<meta http-equiv='referfesh' content='$delay; url=$url'>
+<meta http-equiv='refresh' content='$delay; url=$url'>
 </tr>
 </table>
 </body>";

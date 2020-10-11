68c2b3064691e6c9f364cbecb1e5ef3c8b3ba9c2
bandacittadinadicornedo <62946832+bandacittadinadicornedo@users.noreply.github.com>
2020-10-11 16:40:42 +0200

    Add files via upload

--- invioform.php
+++ invioform.php
@@ -1,5 +1,5 @@
-<?php
-//Recupero i valori inseriti nel form
+﻿<?php
+// Recupero i valori inseriti nel form
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
@@ -8,40 +8,39 @@ $message = $_POST['message'];
 $delay = "4";
 $url = "https://dentalprotesi.github.io";
 
-//verifico che tutti i campi siano stati compilati
-if (!$name || !$email || !$message) {
-	echo 'Tutti i campi del modulo sono obbligatori!';
-	}
-
-//Verifico che il nome non contenga caratteri nocivi
-elseif (!preg_match('/^[A-Aa-z \'-]+$/i',$name)) {
-	echo 'Il nome contiene caratteri non ammessi';
-	}
-	
-//Verifico se un indirizzo email è valido
+// verifico che tutti i campi siano stati compilati
+if (!$nome || !$email || !$tel || !$corso || !$msg) {
+  echo 'Tutti i campi del modulo sono obbligatori!';    
+}
+// verifico che il nome non contenga caratteri nocivi
+elseif (!preg_match('/^[A-Za-z \'-]+$/i',$nome)) {
+  echo 'Il nome contiene caratteri non ammessi';    
+}
+// verifico se un indirizzo email è valido
 elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	echo 'Indirizzo email non corretto';
-	}else{
-		
+  echo 'Indirizzo email non corretto';
+}else{
+ 
 // compilo un messaggio combinando i dati recuperati dal form
 $testo = "Nome: " . $name . "\n"
-		. "Email: " . $email . "\n"
-		. "Messaggio: " . $message;
-		
-// uso la funzione mail di PHP per inviare i dati al mio indirizzo di posta
-mail('lucabee@libero.it', 'Mail da Dental Protesi', $testo);
+       . "Email: " . $email . "\n"
+       . "Messaggio: " . $message;
 
-//Mostro un messaggio di conferma all'utente
+// uso la funzione mail di PHP per inviare questi dati al mio indirizzo di posta
+mail('pacchiarotti@gmail.com', 'Mail da Corsi Excel Roma', $testo);
+
+// Mostro un messaggio di conferma all'utente
 print "<body>
 <table align='center' bordercolor='#CCCCCC'>
   <tr>
     <td>
 <div align='center'><font face='Verdana, Arial, Helvetica, sans-serif'>
-Grazie per averci contattato</font><br>
-Per tornare all'Home page attendi 4 secondi o <a href='https://dentalprotesi.github.io'>Clicca qui</a></font></div></td>
-<meta http-equiv='refresh' content='$delay; url=$url'>
+Grazie per averci contattato</font><br><br>
+Uno dei nostri consulenti le risponderà al più presto all'indirizzo $email<br><br>		
+Per tornare all'Home attendi 4 secondi o <a href='https://dentalprotesi.github.io'>Clicca qui</a></font></div></td>
+<meta http-equiv='refresh' content='$delay;  url=$url'>
 </tr>
 </table>
 </body>";
-	}
-	?>
+}
+?>
